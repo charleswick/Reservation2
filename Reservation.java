eb097d490b70480ca049cc994e08c295ed00368e
charleswick <charles_wick25@milton.edu>
2023-10-11 10:51:27 -0400

    Initial commit

---
+++ Reservation.java
@@ -0,0 +1,19 @@
+public class Reservation {
+    // this will help me make objects of type reservatiom
+    public int numberPeople;
+    public double time;
+    public long timeMade;
+    public int priority;
+
+    public Reservation(int pnumberPeople, double pTime, long ptimeMade, int pPriority){
+        numberPeople=pnumberPeople;
+        time=pTime;
+        timeMade = ptimeMade;
+        priority=pPriority;
+    }
+
+    public void makeString(){
+        System.out.println("Reservation for "+numberPeople+" people at "+time+", made at "+timeMade+", with a priority of "+priority+".");
+
+    }
+}
---
+++ ReservationSystem.java
@@ -0,0 +1,10 @@
+public class ReservationSystem {
+    public static void main(String[] args) {
+        ReservationSystem Jarvis = new ReservationSystem();
+    }
+    public ReservationSystem(){
+        Reservation Gavin = new Reservation(4,10.00,800,1);
+        Gavin.makeString();
+
+    }
+}
