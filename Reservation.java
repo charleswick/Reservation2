32f870fb698aa311b98a3003904b5831655688b0
charleswick <charles_wick25@milton.edu>
2023-11-02 15:19:20 -0400

    inclass added menu mainly

--- Reservation.java
+++ Reservation.java
@@ -1,12 +1,19 @@
 public class Reservation {
     // this will help me make objects of type reservatiom
     public int numberPeople;
+    public String name;
     public double time;
     public long timeMade;
     public int priority;
     public int numReservation;
+    public boolean highTop;
+    public boolean outdoor;
+    //hightop limit is 4
+    //make a waitlist, do you want to do standard?
+    //special occaasion?
 
-    public Reservation(int pnumberPeople, double pTime, long ptimeMade, int pPriority){
+
+    public Reservation(int pnumberPeople, double pTime, long ptimeMade, int pPriority, String name){
         numberPeople=pnumberPeople;
         time=pTime;
         timeMade = ptimeMade;
@@ -15,7 +22,7 @@ public class Reservation {
     }
 
     public void makeString(){
-        System.out.println("Reservation for "+numberPeople+" people at "+time+", made at "+timeMade+", with a priority of "+priority+".");
+        System.out.println("Reservation for "+name+","+numberPeople+" people at "+time+", made at "+timeMade+", with a priority of "+priority+".");
 
     }
 }
--- ReservationSystem.java
+++ ReservationSystem.java
@@ -1,21 +1,17 @@
+import java.util.Scanner;
 public class ReservationSystem {
     Reservation[] reservations = new Reservation[10];
     int reservationsMade;
-
 
 
     public static void main(String[] args) {
         ReservationSystem Jarvis = new ReservationSystem();
 
 
-
-
-
     }
 
 
-
-    public ReservationSystem(){
+    public ReservationSystem() {
 //        Reservation Gavin = new Reservation(4,10.00,800,1);
 //        Gavin.makeString();
 //
@@ -24,119 +20,213 @@ public class ReservationSystem {
 //
 //        printReservations();
 
-            addReservation(10,10,8,80000000);
-
+        addReservation(10, 10, 8, 80000000,"Jerry");
 
 
         printReservations();
+        sortReservations();
+        System.out.println("--------------------------------------");
+        printReservations();
+        System.out.println("--------------------------------------");
+        addReservation(10, 10, 8, 80000000, "Tim");
+        addReservation(10, 10, 8, 80000000, "Test");
+        addReservation(10, 10, 5, 80000000, "Jill");
+        addReservation(10, 10, 9, 80000000, "Tom");
 
-        selectionSort();
 
+        sortReservations();
+        printReservations();
+        boolean contiunueeee = true;
+        while (contiunueeee == true){
+            Scanner starter = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Enter 1 for sort reservation, 2 for print reservation, 3 for add reservation");
 
-
-
-
-        }
-
-//        public void addReservation(int pNumPeople, double pTime, int pPriority, long pTimeMade){
-//            reservations[reservationsMade]=new Reservation(pNumPeople,pTime,pTimeMade,pPriority);
-//            reservationsMade++;
-//
-//
-//
-//
-//
-//
-//
-//
-//    }
-    public void printReservations() {
-        for (int i = 0; i < reservations.length; i++) {
-            if(reservations[i]==null) {
-                System.out.println("empty res");
-
+            int response = Integer.parseInt(starter.nextLine());
+            if(response==1){
+                sortReservations();
             }
-            
+            if(response==2){
+                printReservations();
+            }
+            if(response==3){
+                addUserReservation();
+            }
 
-           // reservations[i].makeString();
+
+            Scanner stopper = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Enter 1 to end program, 2 for continue");
+
+            int response2 = Integer.parseInt(starter.nextLine());
+            if (response2==1){
+                contiunueeee=false;
+            }
+
+
 
 
         }
 
+        sortReservations();
+        printReservations();
 
 
 
 
     }
-    public void sortReservations(){
-        int n =reservations.length;
 
-        for(int i=0; i<n-1;i++){
+    public void selectionSort() {
+        int[] waffles = new int[40];
+        for (int x = 0; x < waffles.length; x++) {
+            waffles[x] = (int) (Math.random() * 100);
+            System.out.print(waffles[x] + ", ");
+        }
+
+        int n = waffles.length;
+
+        for (int i = 0; i < n - 1; i++) {
             int min_int = i;
             //find min int location
-            for(int j=i+1;j<n;j++){
-                if(reservations[j].priority<reservations[min_int].priority)
-                    min_int=j;
+            for (int j = i + 1; j < n; j++) {
+                if (waffles[j] < waffles[min_int])
+                    min_int = j;
 
 
                 //swap values
-                int temp = reservations[min_int].priority;
-                reservations[min_int].priority=reservations[i].priority;
-                reservations[i].priority=temp;
+                int temp = waffles[min_int];
+                waffles[min_int] = waffles[i];
+                waffles[i] = temp;
 
 
             }
 
 
         }
-        public void selectionSort(){
-            int[] waffles = new int[40];
-            for(int x=0;x<waffles.length;x++){
-                waffles[x]=(int)(Math.random()*100);
-                System.out.print(waffles[x]+", ");
+        System.out.println();
+        for (int x = 0; x < waffles.length; x++) {
+
+            System.out.print(waffles[x] + ", ");
+        }
+
+
+    }
+
+
+//    public void addReservation(int pNumPeople, double pTime, int pPriority, long pTimeMade) {
+//        reservations[reservationsMade] = new Reservation(pNumPeople, pTime, pTimeMade, pPriority);
+//        reservationsMade++;
+//
+//
+//    }
+
+        public void printReservations() {
+        for (int i = 0; i < reservations.length; i++) {
+            if(reservations[i]==null) {
+                System.out.println("empty res");
+
             }
 
-            int n =waffles.length;
+else {
+                reservations[i].makeString();
+            }
 
-            for(int i=0; i<n-1;i++){
-                int min_int = i;
-                //find min int location
-                for(int j=i+1;j<n;j++){
-                    if(waffles[j]<waffles[min_int])
-                        min_int=j;
+        }
+//
+//
+//
+//
+//
+    }
+    public void sortReservations() {
+        int n = reservations.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int min_int = i;
+            //find min int location
+
+            for (int j = i + 1; j < n; j++) {
+                if (reservations[j]!= null)
+
+                    if (reservations[j].priority < reservations[min_int].priority)
+                        min_int = j;
 
 
                     //swap values
-                    int temp = waffles[min_int];
-                    waffles[min_int]=waffles[i];
-                    waffles[i]=temp;
-
+                if (reservations[j]!=null){
+                    int temp = reservations[min_int].priority;
+                    reservations[min_int].priority = reservations[i].priority;
+                    reservations[i].priority = temp;
 
                 }
 
 
 
             }
-            System.out.println();
-            for(int x=0;x<waffles.length;x++){
-
-                System.out.print(waffles[x]+", ");
-            }
 
 
-        }    public void addReservation(int pNumPeople, double pTime, int pPriority, long pTimeMade){
-        Reservation chair = new Reservation(pNumPeople,pTime,pTimeMade,pPriority);
-        for(int x =0; x<reservations.length;x++){
-            if(reservations[x]==null){
+        }
+    }
+
+
+    public void addReservation(int pNumPeople, double pTime, int pPriority, long pTimeMade, String pName) {
+        Reservation chair = new Reservation(pNumPeople, pTime, pTimeMade, pPriority, pName);
+        for (int x = 0; x < reservations.length; x++) {
+            if (reservations[x] == null) {
                 System.out.println("empty");
-                reservations[x]=chair;
+                reservations[x] = chair;
                 reservations[x].makeString();
                 break;
 
             }
         }
+
+
     }
+    public void changeReservationTime(String cancelname){
+
+    }
+    public void addUserReservation() {
+
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Enter username");
+
+            String inputname = myObj.nextLine();
 
 
+        Scanner myObj2 = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter number of people");
+
+        int numP = Integer.parseInt(myObj2.nextLine());
+        Scanner myObj3 = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter time of reservation");
+
+        double userTime = (myObj3.nextDouble());
+
+
+        long timeMade =  System.currentTimeMillis();
+
+        int Priority = (int) (Math.random()*10);
+        Reservation tom = new Reservation(numP,userTime,timeMade,Priority,inputname);
+        for (int x = 0; x < reservations.length; x++) {
+            if (reservations[x] == null) {
+                System.out.println("empty");
+                reservations[x] = tom;
+                if (x >=reservations.length){
+                    System.out.println("Array is full!");
+                }
+                //reservations[x].makeString();
+                break;
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
 
 }
